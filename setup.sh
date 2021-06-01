4941b9c1e3cd454c9ff0be96687b40c13733e28c
Vo Duy Khiem <khiem.vo@myob.com>
2021-06-01 23:42:22 +1200

    fix failed build

--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ echo -e "Yay! It all worked! 👍\n"
 echo "BUILDKITE_PIPELINE_SIT_AWS_KEY: $BUILDKITE_PIPELINE_SIT_AWS_KEY"
 echo "BUILDKITE_PIPELINE_SIT_AWS_SECRET: $BUILDKITE_PIPELINE_SIT_AWS_SECRET"
 
+node -v
+
 serverless config credentials --overwrite --provider aws --key AKIASBCQKNZLFOMPGV4M --secret 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
 sls deploy -v
