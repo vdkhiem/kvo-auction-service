5ec137918c0d437844fa27d4742c848aed7ff523
Vo Duy Khiem <khiem.vo@myob.com>
2021-06-13 14:14:19 +1200

    add DynamoDB auction table

--- auction-api/auction/healthcheck.js
+++ auction-api/auction/healthcheck.js
@@ -1,7 +1,12 @@
+// event: contains body query parameter, path parameter and other headers, etc...
+// context: contains some meta-data about function lambda
+// can add custom data to event, and context via middleware
+// authentication, can put id to context via middleware
 async function healthcheck(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Health check status OK.' }),
+    // body: JSON.stringify({event, context}), // To see detail of event and context
+    body: JSON.stringify({ message: 'Health check status OK.' }), // the body must wrap around JSON.stringify, otherwise we get bunch of error
   };
 }
 
